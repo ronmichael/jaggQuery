546634c758ea929780e522d6c199a49bf97c3f30
Ron Michael Zettlemoyer <ron@zettlemoyer.com>
2014-10-29 12:15:42 -0400

    Update jaggquery.js

--- jaggquery.js
+++ jaggquery.js
@@ -1,8 +1,29 @@
 
-
 $.fn.sum = function () {
-    var sum=0, idx=0, last=this.length;
-    for(;idx<last;idx++) sum += parseFloat(this[idx].value);
+    var sum = 0, idx = 0, last = this.length;
+    for(; idx < last; idx++) {
+        var v = parseFloat(this[idx].value);
+        if(!isNaN(v)) sum += v;
+    }
     return sum;
 };
 
+
+$.fn.min = function () {
+    var min = null, idx = 0, last = this.length;
+    for (; idx < last; idx++) {
+        var v = parseFloat(this[idx].value);
+        if(!isNaN(v) && (min===null || v<min)) min = v;
+    }
+    return min;
+};
+
+
+$.fn.max = function () {
+    var min = null, idx = 0, last = this.length;
+    for (; idx < last; idx++) {
+        var v = parseFloat(this[idx].value);
+        if(!isNaN(v) && (min===null || v>min)) min = v;
+    }
+    return min;
+};
